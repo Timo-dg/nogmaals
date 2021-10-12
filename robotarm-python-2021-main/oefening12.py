0b399e54fa035d846c60cc9f1029cb7f26eba6c6
Timo de Gruiter <timo@EDGITNEW.local>
2021-10-12 10:24:50 +0200

    oefening 12 afgerond

---
+++ robotarm-python-2021-main/oefening12.py
@@ -0,0 +1,19 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 12')
+robotArm.speed = 2
+# Jouw python instructies zet je vanaf hier:
+for x in range(9, 0, -1):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == 'red':
+        for y in range(x):
+            robotArm.moveRight()
+        robotArm.drop()
+        for y in range(x):
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+    robotArm.moveRight()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
